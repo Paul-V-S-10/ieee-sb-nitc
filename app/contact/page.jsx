ed5bf3e71a8e1fd922a154dca1c0c5c935e397b3
simonM21 <simonmattekkatt21@gmail.com>
2025-07-04 10:53:31 +0530

    did the bacis frame of the page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -8,44 +8,57 @@ const page = () => {
         <h1>Contact Us</h1>
         <p>Get in Touch with IEEE SB NITC</p>
       </div>
+
       <div className={styles.container}>
-        <div className={styles.formContainer}>
-          <h2>SEND A MESSAGE</h2>
-          <div className={styles.formGroup}>
-            <label>Name:</label>
-            <input type="text" />
+        <div className={styles.formCardBox}>
+          <div className={styles.formCard}>
+            <h2>SEND A MESSAGE</h2>
+
+            <div className={styles.row}>
+              <div className={styles.inputGroup}>
+                <label className={styles.label}>Name:</label>
+                <input type="text" className={styles.input} />
+              </div>
+
+              <div className={styles.inputGroup}>
+                <label className={styles.label}>Email:</label>
+                <input type="email" className={styles.input} />
+              </div>
+            </div>
+
+            <div className={styles.inputGroup}>
+              <label className={styles.label}>Subject:</label>
+              <input type="text" className={styles.input} />
+            </div>
+
+            <div className={styles.inputGroup}>
+              <label className={styles.label}>Messages:</label>
+              <textarea className={styles.textarea}></textarea>
+            </div>
+
+            <button className={styles.button}>Contact Us</button>
           </div>
-          <div className={styles.formGroup}>
-            <label>Email:</label>
-            <input type="email" />
-          </div>
-          <div className={styles.formGroup}>
-            <label>Subject:</label>
-            <input type="text" />
-          </div>
-          <div className={styles.formGroup}>
-            <label>Messages:</label>
-            <textarea className={styles.textarea} />
-          </div>
-          <button className={styles.button}>Contact Us</button>
         </div>
 
         <div className={styles.contactInfo}>
-          <h3>Contact Information</h3>
-          <p>
-            <strong>Address</strong>
-          </p>
-          <p>
-            Mukkam Road
-            <br />
-            Kattangal
-            <br />
-            Kozhikode, Kerala
-          </p>
-          <p>
-            <strong>E-mail</strong>
-          </p>
-          <p>ieeesb@nitc.ac.in</p>
+          <div className={styles.contactInfoBox}>
+            <h3>Contact Information</h3>
+            <p>
+              <strong>Address</strong>
+            </p>
+            <p>
+              Mukkam Road
+              <br />
+              Kattangal
+              <br />
+              Kozhikode, Kerala
+            </p>
+            <p>
+              <strong>E-mail</strong>
+            </p>
+            <p>ieeesb@nitc.ac.in</p>
+          </div>
+
           <img className={styles.map} src="/nitc_map.png" alt="NITC Map" />
         </div>
       </div>
